c37f336565cfde45f7f5c40aeedf626d20d41c3d
Shiva <shiva220620@gmail.com>
2026-03-19 11:09:02 +0530

    adddigits qs

---
+++ math_q/adddigits.py
@@ -0,0 +1,39 @@
+
+# Code
+# Testcase
+# Testcase
+# Test Result
+# 258. Add Digits
+# Attempted
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+ 
+
+# Example 1:
+
+# Input: num = 38
+# Output: 2
+# Explanation: The process is
+# 38 --> 3 + 8 --> 11
+# 11 --> 1 + 1 --> 2 
+# Since 2 has only one digit, return it.
+# Example 2:
+
+# Input: num = 0
+# Output: 0
+
+num = int(input("Enter the num : "))
+
+while num >= 10:
+    s=0
+    while num>0:
+        s=s+num%10
+        num=num//10
+    num=s
+print(num)
+
