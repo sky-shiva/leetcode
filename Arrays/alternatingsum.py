c0359be743e079e2d77fe5d112c748e315811b1b
Shiva <shiva220620@gmail.com>
2026-03-20 18:53:44 +0530

    Added arrays folder

---
+++ Arrays/alternatingsum.py
@@ -0,0 +1,5 @@
+# You are given an integer array nums.
+
+# The alternating sum of nums is the value obtained by adding elements at even indices and subtracting elements at odd indices. That is, nums[0] - nums[1] + nums[2] - nums[3]...
+
+# Return an integer denoting the alternating sum of nums.
