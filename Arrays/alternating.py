c1bb8bafd9c1e1232816193cb51e7b5eb5afa565
Shiva <shiva220620@gmail.com>
2026-03-20 19:41:10 +0530

    alternating sum

--- Arrays/alternating.py
+++ Arrays/alternating.py
@@ -1,1 +1,22 @@
-hi
+# You are given an integer array nums.
+
+# The alternating sum of nums is the value obtained by adding elements at even indices and subtracting elements at odd indices. That is, nums[0] - nums[1] + nums[2] - nums[3]...
+
+# Return an integer denoting the alternating sum of nums.
+
+n=int(input("Enter the size of an array : "))
+nums=[]
+for i in range(n):
+    num = int(input(f"Enter {n} numbers:  "))
+    nums.append(num)
+
+esum=0
+osum=0
+for i in range(len(nums)):
+    if i%2==0:
+        esum+=nums[i]
+    else:
+        osum+=nums[i]
+print(esum-osum)
+
+    
