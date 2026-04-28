2ecd2c5e755a5734c37e36c74b471e0948367f55
Shiva <shiva220620@gmail.com>
2026-04-28 20:28:33 +0530

    daa 1_2

--- DAA/exp1_2.py
+++ DAA/exp1_2.py
@@ -1,1 +1,11 @@
-g
+def count_set_bits(n):
+    return bin(n).count('1')
+
+n=int(input())
+arr=[]
+
+for i in range(n):
+    num=int(input())
+    arr.append(num)
+    arr.sort(key=count_set_bits,reverse=True)
+print(arr)
