47a9098307a876915f81e62f05de8cb2577b9910
Shiva <shiva220620@gmail.com>
2026-05-30 11:17:41 +0530

    waytoolong

---
+++ codeforces/M.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+n1="I hate it"
+n1s="I hate"
+
+if n==1:
+    print(n1)
+elif n%2==0:
+    print(n1s+" that I love it")
+else:   
+    print(n1s+" that I love it that I hate it")
--- codeforces/waytoolong.py
+++ codeforces/waytoolong.py
@@ -1,9 +1,10 @@
-
 t=int(input())
 
 for _ in range(t):
-    s = input()
+    s= input()
+
     if len(s)>10:
         print(s[0]+str(len(s)-2)+s[-1])
     else:
         print(s)
+
